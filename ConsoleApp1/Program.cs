6ed3cf0e44460fc07dc1d305f2fc350586e22a80
Sylwester Włodyga <Sylwester.w@gmail.com>
2018-11-13 13:33:59 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.Read();
+        }
+    }
+
+    internal class User
+    {
+        Private String name = "jacek";
+        Private String surename = "placek";
+
+        public getUser()
+        {
+            Console.WriteLine(name + surename);
+        }
+    }
+
+
+}
